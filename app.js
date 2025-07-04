5a2dc6656e6e963aa42cd6423fbe8a3f4603d5a4
rileyace <pasicaranriley@gmail.com>
2025-07-03 22:15:06 -0400

    Set up 3 question routes

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
-
 const express = require('express')
 const app = express()
 const port = 3000
 
+const question1 = require('./routes/question1')
+const question2 = require('./routes/question2')
+const question3 = require('./routes/question3')
+
+// Base
 app.get('/', (req, res) => {
-  res.send('Hello World from Express!')
+  res.send('Welcome to the NodeJS/Express Assignment')
 })
 
-app.get('/demo', (req, res) => {
-  res.send(`
-    <html>
-      <head><title>Demo</title></head>
-      <body>
-        <h1>Screen Layout Placeholder</h1>
-      </body>
-    </html>
-  `)
-})
+// Question routes
+app.use('/question1', question1)
+app.use('/question2', question2)
+app.use('/question3', question3)
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`)
 })
-
